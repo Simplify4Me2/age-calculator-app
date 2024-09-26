7fff9c8452ead68cf99a7a6ea30d6331dbf6ba48
Simon <simon.vereecke@live.com>
2024-09-26 22:41:16 +0200

    add date validation

---
+++ AgeCalculatorApp/Validators/ValidDateAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Input =  AgeCalculatorApp.Layout.MainLayout.Input;
+
+namespace AgeCalculatorApp.Validators
+{
+    public class ValidDateAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is null)
+                return new ValidationResult("This field is required");
+
+            var day = (int) value;
+
+            if (day < 1 || day > 31)
+                return new ValidationResult("Must be a valid day");
+
+            Input model = (Input)validationContext.ObjectInstance;
+
+            if (model is null || model.Month is null || model.Year is null)
+                return ValidationResult.Success;
+
+            var isValidDate = DateTime.TryParse($"{value}/{model.Month}/{model.Year}", out DateTime date);
+
+            if (!isValidDate)
+                return new ValidationResult("Must be a valid date");
+            
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ AgeCalculatorApp/Validators/YearRangeAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgeCalculatorApp.Validators
+{
+    public class YearRangeAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is null)
+                return ValidationResult.Success;
+
+            int currentYear = DateTime.Today.Year;
+            if (value is int year)
+            {
+                if (year < 1 || year > currentYear)
+                    return new ValidationResult("Must be in the past");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
